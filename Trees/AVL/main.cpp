fb7215733d6a49bbd7775d4550ed477f4ce30a70
Zaim Abbasi <zaim.k.abbasi@gmail.com>
2023-12-13 14:36:44 +0500

    implementing insert in AVL

--- Trees/AVL/main.cpp
+++ Trees/AVL/main.cpp
@@ -7,3 +7,40 @@ public:
     Node *right;
     int height;
 };
+class AVL{
+public:
+    Node *root;
+    AVL()
+    {
+        root = NULL;
+    }
+
+    int height(Node* t)
+    {
+        if(t == NULL)
+            return -1;
+        return t->height;
+    }
+    Node* insert(int x, Node* root)
+    {
+        if (root == NULL)
+        {
+            root = new Node();
+            root->data = x;
+            root->height = 0;
+            root->left = root->right = NULL;
+        }
+
+        else if(x < root->data)
+        {
+            // yani ke left me jana ha
+            root->left = insert(x, root->left);
+
+            if(height(root->left) - height(root->right) == 2)
+            {
+                
+            }
+        }
+
+    }
+};

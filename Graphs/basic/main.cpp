039feec442a65c9e5242fe667f2f3c9dc9b91249
Zaim Abbasi <zaim.k.abbasi@gmail.com>
2023-12-18 21:15:23 +0500

    Refactor code to improve performance and readability

--- Graphs/basic/main.cpp
+++ Graphs/basic/main.cpp
@@ -1,6 +1,101 @@
-#include "queue.h"
+// implementation of a graph using adjacency matrix
+
+#include <iostream>
+using namespace std;
+
+class Graph
+{
+public:
+    int **adjMatrix;
+    int numVertices;
+
+    Graph(int numVertices)
+    {
+        this->numVertices = numVertices;
+        adjMatrix = new int *[numVertices];
+        for (int i = 0; i < numVertices; i++)
+        {
+            adjMatrix[i] = new int[numVertices];
+            for (int j = 0; j < numVertices; j++)
+            {
+                adjMatrix[i][j] = 0;
+            }
+        }
+    }
+
+    void addEdge(int i, int j)
+    {
+        adjMatrix[i][j] = 1;
+        adjMatrix[j][i] = 1;
+    }
+
+    void removeEdge(int i, int j)
+    {
+        adjMatrix[i][j] = 0;
+        adjMatrix[j][i] = 0;
+    }
+
+    void print()
+    {
+        for (int i = 0; i < numVertices; i++)
+        {
+            cout << i << " : ";
+            for (int j = 0; j < numVertices; j++)
+            {
+                cout << adjMatrix[i][j] << " ";
+            }
+            cout << endl;
+        }
+    }
+
+    void PrintMaze()
+    {
+        for (int i = 0; i < numVertices; i++)
+        {
+            cout << i << " : ";
+            for (int j = 0; j < numVertices; j++)
+            {
+                if (adjMatrix[i][j] == 1)
+                {
+                    cout << "X ";
+                }
+                else
+                {
+                    cout << "  ";
+                }
+            }
+            cout << endl;
+        }
+
+    }
+
+    ~Graph()
+    {
+        for (int i = 0; i < numVertices; i++)
+        {
+            delete[] adjMatrix[i];
+        }
+        delete[] adjMatrix;
+    }
+};
+
 int main()
 {
-    Queue q(400);
+    // hard code to make a 2 by 2 maze like structure using some specific formula to generate a new graph each time the program is executed. Also there should be a path from first node to last node.
+
+    Graph g(4);
+    g.addEdge(0, 1);
+    g.addEdge(1, 2);
+    g.addEdge(2, 3);
+    // g.addEdge(3, 0);
+    g.addEdge(0, 2);
+    g.addEdge(1, 3);
     
+    
+
+
+    g.PrintMaze();
+
+
+    return 0;
 }

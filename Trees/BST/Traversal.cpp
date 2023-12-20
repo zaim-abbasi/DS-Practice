438fc605f63fd04ec6bfd9e2baf61da3d2e62e5b
Zaim Abbasi <zaim.k.abbasi@gmail.com>
2023-12-20 17:16:01 +0500

    Refactor BST traversal code

--- Trees/BST/Traversal.cpp
+++ Trees/BST/Traversal.cpp
@@ -1,37 +1,41 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-class Node{
+class Node
+{
 public:
     int data;
     Node *left;
     Node *right;
-    Node(int d){
+    Node(int d)
+    {
         data = d;
         left = NULL;
         right = NULL;
     }
 };
-void PreOrder(Node *root){
-    if(root == NULL){
+void PreOrder(Node *root)
+{
+    if (root == NULL)
+    {
         return;
     }
-    cout<<root->data<<" ";
+    cout << root->data << " ";
     PreOrder(root->left);
     PreOrder(root->right);
 }
 
-void PostOrder(Node* root)
+void PostOrder(Node *root)
 {
-    if(root == NULL)
+    if (root == NULL)
         return;
     PostOrder(root->left);
     PostOrder(root->right);
     cout << root->data << " ";
 }
 
-void Inorder(Node* root)
+void Inorder(Node *root)
 {
-    if(root == NULL)
+    if (root == NULL)
         return;
     Inorder(root->left);
     cout << root->data << " ";
@@ -42,7 +46,7 @@ int main()
     Node *root = new Node(10);
     root->left = new Node(20);
     root->right = new Node(30);
-    
+
     PreOrder(root);
     return 0;
 }
